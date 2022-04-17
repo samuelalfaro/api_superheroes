0ea80a033f86caf6883abb0de408d3322bd5d4af
salfaro <samuelalfaro@gmail.com>
2022-04-17 16:46:28 +0200

    Se añade la BDD H2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO TBL_SUPERHEROES (
+   id,  nombre
+) VALUES (
+--La era de los Minutemen
+   12, 'Espectro de Seda'
+),(13, 'Capitán Metrópolis'
+),(11, 'Búho Nocturno'
+),(14, 'Justicia Encapuchada'
+),(15, 'Polilla'
+),(16, 'Dólar Bill'
+),(17, 'Silueta'
+--La era de los Vigilantes
+),(21, 'El Comediante'
+),(22, 'Rorschach'
+),(23, 'Ozymandias'
+),(24, 'Doctor Manhattan'
+),(25, 'Espectro de Seda II'
+),(26, 'Búho Nocturno II'
+);
